dea18eae5474c1990cabb160ae091b2c3a6063af
pef <RyanPeffer@iCloud.com>
2022-03-18 01:51:52 -0400

    troubleshooting mongo db atlas deploy issue

---
+++ client/build/precache-manifest.09f80d87797d92d15cb706685057f378.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d37498f5092089168ee7fde7f54c66",
+    "url": "/Linktree-Clone/index.html"
+  },
+  {
+    "revision": "7a65b5d76148c4033581",
+    "url": "/Linktree-Clone/static/css/main.cef788ea.chunk.css"
+  },
+  {
+    "revision": "5b160ae3cac58bf6d2f1",
+    "url": "/Linktree-Clone/static/js/2.e2e1a76c.chunk.js"
+  },
+  {
+    "revision": "7a65b5d76148c4033581",
+    "url": "/Linktree-Clone/static/js/main.700aba61.chunk.js"
+  },
+  {
+    "revision": "6b12a6764069d226c5e0",
+    "url": "/Linktree-Clone/static/js/runtime~main.7ce5f81a.js"
+  },
+  {
+    "revision": "6a69c88bac630144c49fca24647ea136",
+    "url": "/Linktree-Clone/static/media/default.6a69c88b.png"
+  },
+  {
+    "revision": "320800a3f58789983df5d78693197344",
+    "url": "/Linktree-Clone/static/media/edit.320800a3.svg"
+  },
+  {
+    "revision": "b740e4d131d48d0b1477c2eb9e082622",
+    "url": "/Linktree-Clone/static/media/trash.b740e4d1.svg"
+  }
+]);
